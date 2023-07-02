9e61eebe147f28087be712b114f2d4593da9ecf8
Farmlanddd <138355399+Farmlanddd@users.noreply.github.com>
2023-07-02 13:45:04 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'Dankee babee mwehehe, tunggu yhh GA ANEH ANEH KOK. I sayanggg bangett sama uuu :3 I LOVE YOUUU';
+});
+
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
